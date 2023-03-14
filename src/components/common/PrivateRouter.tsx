320ad53262e486c5acf70acd0807467a7c7f4bdc
autumnly1007 <autumnly1007@gmail.com>
2023-03-14 15:03:10 +0900

    Env: 경로 보호 HOC  추가

--- src/components/common/PrivateRouter.tsx
+++ src/components/common/PrivateRouter.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { useRouter } from 'next/router';
-import { NextPage } from 'next';
 import { getCookie } from '@/utils/cookie';
 import Login from '../../../pages/login';
 
-const withAuth = (Component: NextPage | React.FC) => {
+const withAuth = (Component: React.FC<any>) => {
   const Auth = () => {
-    const router = useRouter();
     const accessToken = getCookie('accessToken');
 
     if (!accessToken) {
-      router.replace('/');
       return <Login />;
     }
     return <Component />;
