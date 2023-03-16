1604377211640e8848c63e838e90de4143832f71
autumnly1007 <autumnly1007@gmail.com>
2023-03-16 20:25:58 +0900

    Feat: 최근 검색어 저장 기능 구현

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,7 +1,83 @@
 import React from 'react';
+import { RootState } from '@/store';
+import { default as ReactModal } from 'react-modal';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import Button from '../common/Button';
 
 const Modal = () => {
-  return <div>Modal</div>;
+  const modalState = useSelector((state: RootState) => state.modal);
+
+  const customStyles = {
+    content: {
+      width: '250px',
+      height: '160px',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      padding: '10px',
+      borderRadius: '10px',
+      backgroundColor: 'white',
+      boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
+      zIndex: '100',
+    },
+  };
+
+  return (
+    <ReactModal isOpen={modalState.isOpen} style={customStyles}>
+      <ModalWrap>
+        <ModalText>
+          <p>{modalState.text}</p>
+        </ModalText>
+        <ButtonWrap>
+          <Button
+            variant="blue"
+            width="80px"
+            height="34px"
+            borderRadius="6px"
+            onClick={modalState.onClickOk}
+          >
+            {modalState.okText ? modalState.okText : '확인'}
+          </Button>
+          {modalState.onClickCancel && (
+            <Button
+              variant="white"
+              width={modalState.cancelText ? '110px' : '80px'}
+              height="34px"
+              borderRadius="6px"
+              onClick={modalState.onClickCancel}
+            >
+              {modalState.cancelText ? modalState.cancelText : '취소'}
+            </Button>
+          )}
+        </ButtonWrap>
+      </ModalWrap>
+    </ReactModal>
+  );
 };
 
 export default Modal;
+
+const ModalWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ModalText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  line-height: 1.6;
+  white-space: pre-wrap;
+  height: 80px;
+  padding: 16px 0 12px 0;
+`;
+
+const ButtonWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`;
