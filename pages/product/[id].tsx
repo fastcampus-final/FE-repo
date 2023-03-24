304f74e3a62f8bf2149ffb25c4e31d728d514d73
plou102 <plou102@naver.com>
2023-03-24 15:59:07 +0900

    Feat: 상세 페이지 옵션 선택 파트 구현

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import styled from '@emotion/styled';
 import Image from '@/../../src/components/common/Image';
 import { IDetail } from '@/interfaces/product';
 import { formatPrice } from '@/utils/format';
-import { AiOutlineHeart } from 'react-icons/ai';
 
-import Select from 'react-select';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { RxCrossCircled } from 'react-icons/rx';
+
+import Select, { OnChangeValue } from 'react-select';
 
 const tempData: IDetail = {
   productId: '1',
@@ -20,12 +22,27 @@ const tempData: IDetail = {
   airline: '인천-트빌리시 왕복 항공',
 };
 
+interface IItemOption {
+  optionDate: string;
+}
+
+interface ICountType {
+  count: number;
+}
+
 const ProductDetail = () => {
   const router = useRouter();
-  const [date, setDate] = useState<string>();
+
   const [single, setSingle] = useState('');
-  const [items, setItems] = useState([]);
-  const [counts, setCounts] = useState(1);
+  const [singleCount, setSingleCount] = useState(1);
+
+  const [items, setItems] = useState<Array<IItemOption>>([]);
+  const [itemCounts, setItemCounts] = useState<Array<ICountType>>([]);
+
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalMember, setTotalMember] = useState(0);
+
+  const productPrice = Number(tempData.price);
 
   const DateOptions = [
     {
@@ -46,21 +63,75 @@ const ProductDetail = () => {
     },
   ];
 
-  const dateSelect = (option: any) => {
-    // setDate(option.value);
-    const optionDate = option.label;
-    const count = option.value;
+  const SingleOption = [
+    {
+      value: 1,
+      label: `1인 싱글룸 사용시 추가 ${formatPrice('540000')}`,
+    },
+  ];
 
-    const newItem = {
-      optionDate,
-      count,
-    };
+  const dateSelect = (option: OnChangeValue<{ value: number; label: string }, false>) => {
+    const optionDate = option?.label as string;
+    const count = option?.value as number;
 
-    setItems([newItem as never, ...items]);
+    if (items.some((item) => item.optionDate === optionDate)) {
+      alert('이미 선택하신 옵션입니다.');
+    } else {
+      const newItem = {
+        optionDate,
+      };
+
+      const newCount = {
+        count,
+      };
+
+      setItems([newItem, ...items]);
+      setItemCounts([newCount, ...itemCounts]);
+      setTotalPrice(totalPrice + productPrice * count);
+      setTotalMember(totalMember + count);
+    }
   };
 
-  const singleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSingle(e.target.value);
+  const singleSelect = (option: OnChangeValue<{ value: number; label: string }, false>) => {
+    if (single !== '') {
+      setSingleCount(singleCount + 1);
+      setTotalPrice(totalPrice + 540000);
+    } else {
+      setSingle(option?.label as string);
+      setSingleCount(option?.value as number);
+      setTotalPrice(totalPrice + 540000);
+    }
+  };
+
+  const plusClick = (i: number) => {
+    const plus = [...itemCounts];
+    plus[i].count += 1;
+    setItemCounts([...plus]);
+
+    setTotalPrice(totalPrice + productPrice);
+    setTotalMember(totalMember + 1);
+  };
+
+  const minusClick = (i: number) => {
+    const minus = [...itemCounts];
+    minus[i].count -= 1;
+    setItemCounts([...minus]);
+
+    setTotalPrice(totalPrice - productPrice);
+    setTotalMember(totalMember - 1);
+  };
+
+  const removeItem = (i: number) => {
+    setTotalPrice(totalPrice - productPrice * itemCounts[i]?.count);
+    setTotalMember(totalMember - itemCounts[i]?.count);
+
+    const itemRemove = [...items];
+    itemRemove.splice(i, 1);
+    setItems([...itemRemove]);
+
+    const countRemove = [...itemCounts];
+    countRemove.splice(i, 1);
+    setItemCounts([...countRemove]);
   };
 
   return (
@@ -86,40 +157,40 @@ const ProductDetail = () => {
           />
 
           <p className="dropTitle">싱글차지</p>
-          <select onChange={singleSelect}>
-            <option selected hidden disabled>
-              싱글룸을 이용하려면 선택해 주세요 (선택)
-            </option>
-            <option value="1인 싱글룸 사용시 추가">
-              1인 싱글룸 사용시 추가 {formatPrice('540000')}
-            </option>
-          </select>
+          <Select
+            onChange={singleSelect}
+            options={SingleOption}
+            placeholder="싱글룸을 이용하려면 선택해 주세요 (선택)"
+            className="singleSelect"
+          />
 
           {items.length > 0
-            ? items.map((item: { optionDate: string; count: number }, i: React.Key) => {
+            ? items.map((item, i: number) => {
                 return (
                   <ItemContent key={i}>
-                    <p className="itemTitle">{item?.optionDate}</p>
+                    <p className="itemTitle">
+                      {item?.optionDate} <RxCrossCircled onClick={() => removeItem(i)} />
+                    </p>
                     <button
                       onClick={() => {
-                        if (item.count > 1) {
-                          item.count - 1;
+                        if (itemCounts[i].count > 1) {
+                          minusClick(i);
                         }
                       }}
                     >
                       -
                     </button>
-                    <p>{item.count}</p>
+                    <p className="count">{itemCounts[i].count}</p>
                     <button
                       onClick={() => {
-                        item.count += 1;
-                        console.log(item.count);
-                        // setCounts(item.count + 1);
+                        plusClick(i);
                       }}
                     >
                       +
                     </button>
-                    <p>{formatPrice(tempData.price)}</p>
+                    <p className="itemPrice">
+                      {formatPrice(`${productPrice * itemCounts[i].count}`)}
+                    </p>
                   </ItemContent>
                 );
               })
@@ -127,9 +198,43 @@ const ProductDetail = () => {
 
           {single !== '' ? (
             <ItemContent>
-              <p>{single}</p>
-              <p>{formatPrice('540000')}</p>
+              <p className="itemTitle">
+                {single}{' '}
+                <RxCrossCircled
+                  onClick={() => {
+                    setTotalPrice(totalPrice - 540000 * singleCount);
+                    setSingle('');
+                  }}
+                />
+              </p>
+              <button
+                onClick={() => {
+                  if (singleCount > 1) {
+                    setSingleCount(singleCount - 1);
+                    setTotalPrice(totalPrice - 540000);
+                  }
+                }}
+              >
+                -
+              </button>
+              <p className="count">{singleCount}</p>
+              <button
+                onClick={() => {
+                  setSingleCount(singleCount + 1);
+                  setTotalPrice(totalPrice + 540000);
+                }}
+              >
+                +
+              </button>
+              <p className="itemPrice">{formatPrice(`${540000 * singleCount}`)}</p>
             </ItemContent>
+          ) : null}
+
+          {items.length > 0 || single !== '' ? (
+            <TotalContent>
+              <p>총 상품금액 ( {totalMember} 명 )</p>
+              <p className="totalPrice">{formatPrice(`${totalPrice}`)}</p>
+            </TotalContent>
           ) : null}
 
           <button>예약하기</button>
@@ -188,17 +293,43 @@ const TextContent = styled.div`
     font-weight: 500;
     margin: 36px 0 10px;
   }
-  select,
-  input {
-    width: 100%;
-    font-size: 1rem;
-    padding: 10px;
+  .singleSelect {
+    margin-bottom: 40px;
   }
 `;
 
 const ItemContent = styled.div`
-  margin-top: 24px;
+  margin-top: 20px;
+  padding: 15px;
+  background-color: rgba(33, 37, 41, 0.1);
   .itemTitle {
+    margin-bottom: 15px;
+    padding-bottom: 15px;
     border-bottom: 1px solid #999;
+  }
+  .count {
+    display: inline-block;
+    margin: auto 15px;
+  }
+  .itemPrice {
+    display: inline-block;
+    float: right;
+  }
+  svg {
+    float: right;
+    font-size: 1.2rem;
+  }
+`;
+
+const TotalContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 35px 0;
+  p {
+    margin: auto 0;
+  }
+  .totalPrice {
+    color: #4581f8;
+    font-size: 1.4rem;
   }
 `;
