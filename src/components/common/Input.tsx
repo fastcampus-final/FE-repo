3df7208992d23ccf7c952dfa90bfb474cc79e175
Kim JiYeong <zmswkfl@naver.com>
2023-03-28 19:19:13 +0900

    Fix : fix input error
    - input 에러 수정

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -5,7 +5,7 @@ const Input = ({ error, register, id, type, placeholder, label }: IInputProps) =
   return (
     <div>
       <label htmlFor={id}>{label}</label>
-      <input id={id} type={type} placeholder={placeholder} {...register} value={value} />
+      <input id={id} type={type} placeholder={placeholder} {...register} />
       {error && <div role="alert">{error}</div>}
     </div>
   );
