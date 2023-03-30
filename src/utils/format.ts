42ff49c4877353df2e0919e60dc95ca3c3221cf3
plou102 <plou102@naver.com>
2023-03-30 17:47:43 +0900

    Feat: 커뮤니티 - 여행 후기 목록 구현

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -5,3 +5,8 @@ export const formatPrice = (value: string) => {
 export const formatPeriod = (value: string) => {
   return value.replace(/(\d{4})(\d{2})(\d{2})~(\d{4})(\d{2})(\d{2})/, '$1.$2.$3 ~ $4.$5.$6');
 };
+
+export const formatUserName = (value: string) => {
+  if (value === '관리자') return value;
+  else return value.replace(/(?<=.{2})./gi, '*');
+};
