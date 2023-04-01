4f5c80275ab37b654399ec4f84c0801b241eb8cf
plou102 <plou102@naver.com>
2023-04-01 18:11:23 +0900

    Feat: 뒤로가기 버튼 구현

--- pages/community/review/add.tsx
+++ pages/community/review/add.tsx
@@ -1,7 +1,35 @@
+import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
 import React from 'react';
 
+import ArrowLeft from '@/../public/icons/arrow-left.svg';
+
 const ReviewAdd = () => {
-  return <div>ReviewAdd</div>;
+  const router = useRouter();
+  return (
+    <AddContent>
+      <BackRouter onClick={() => router.back()}>
+        <ArrowLeft />
+        <p>커뮤니티</p>
+      </BackRouter>
+    </AddContent>
+  );
 };
 
 export default ReviewAdd;
+
+const AddContent = styled.div``;
+
+const BackRouter = styled.div`
+  display: flex;
+  padding: 0 1.5rem;
+  svg {
+    width: 30px;
+    margin-right: 10px;
+  }
+  p {
+    margin: auto 0;
+    font-size: 1.3rem;
+    color: #878787;
+  }
+`;
