24cc6a110b9167ffa7e32ea7ed7fd1cbe295f151
plou102 <plou102@naver.com>
2023-04-03 17:54:37 +0900

    Feat: 후기 추가 페이지 구현

--- pages/community/review/add.tsx
+++ pages/community/review/add.tsx
@@ -1,28 +1,100 @@
 import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 
 import ArrowLeft from '@/../public/icons/arrow-left.svg';
+import { Button, TextField } from '@mui/material';
+import { formatUserName } from '@/utils/format';
+import Editor from '@/components/common/Editor';
 
 const ReviewAdd = () => {
   const router = useRouter();
+  const [keyword, setKeyword] = useState('');
+
+  const [editValue, setEditValue] = useState('');
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [fileName, setFileName] = useState('');
+  const [fileUrl, setFileUrl] = useState('');
+
+  const onUploadImage = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      return;
+    }
+    setFileName(e.target.files[0]?.name);
+    setFileUrl(URL.createObjectURL(e.target.files[0]));
+  }, []);
+
+  const onUploadImageButtonClick = useCallback(() => {
+    if (!inputRef.current) {
+      return;
+    }
+    inputRef.current.click();
+  }, []);
+
+  const deleteFileImage = () => {
+    URL.revokeObjectURL(fileUrl);
+    setFileUrl('');
+    setFileName('');
+  };
+
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1 < 10 ? '0' + (today.getMonth() + 1) : today.getMonth() + 1;
+  const date = today.getDate() < 10 ? '0' + today.getDate() : today.getDate();
+
+  const addDate = year + '-' + month + '-' + date;
+
   return (
     <AddContent>
       <BackRouter onClick={() => router.back()}>
         <ArrowLeft />
         <p>커뮤니티</p>
       </BackRouter>
+
+      <TitleContent>
+        <p>제목</p>
+        <TextField
+          placeholder="제목을 입력해 주세요."
+          className="title"
+          value={keyword}
+          onChange={(event) => setKeyword(event.target.value)}
+        />
+      </TitleContent>
+
+      <TitleImage>
+        <Button onClick={onUploadImageButtonClick}>제목 이미지 업로드</Button>
+        <input type="file" ref={inputRef} onChange={onUploadImage} style={{ display: 'none' }} />
+        {fileName}
+        {fileName !== '' ? <Button onClick={() => deleteFileImage()}>지우기</Button> : null}
+      </TitleImage>
+
+      <UserContent>
+        <span className="user">{formatUserName('홍길동')}</span>
+        <span>{addDate}</span>
+      </UserContent>
+
+      <EditorContent>
+        <Editor setEditValue={setEditValue} editValue={editValue} />
+      </EditorContent>
+
+      <ButtonContent>
+        <button className="white">취소</button>
+        <button className="blue">저장</button>
+      </ButtonContent>
     </AddContent>
   );
 };
 
 export default ReviewAdd;
 
-const AddContent = styled.div``;
+const AddContent = styled.div`
+  padding: 0 1.5rem;
+`;
 
 const BackRouter = styled.div`
   display: flex;
-  padding: 0 1.5rem;
+  margin-bottom: 50px;
   svg {
     width: 30px;
     margin-right: 10px;
@@ -31,5 +103,68 @@ const BackRouter = styled.div`
     margin: auto 0;
     font-size: 1.3rem;
     color: #878787;
+  }
+`;
+
+const TitleContent = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+  p {
+    font-size: 1.2rem;
+    color: #878787;
+    margin: auto 15px auto 0;
+  }
+  .title {
+    width: 40%;
+    input {
+      padding: 5px;
+      border: none;
+    }
+  }
+`;
+
+const TitleImage = styled.div``;
+
+const UserContent = styled.div`
+  color: #878787;
+  text-align: right;
+  margin-bottom: 20px;
+  margin-right: 5px;
+  .user {
+    margin-right: 10px;
+  }
+`;
+
+const EditorContent = styled.div`
+  height: 550px;
+  .quill {
+    height: 90%;
+    .ql-container {
+      @media screen and (max-width: 500px) {
+        height: 90%;
+      }
+    }
+  }
+`;
+
+const ButtonContent = styled.div`
+  margin-bottom: 30px;
+  text-align: right;
+  button {
+    border-radius: 8px;
+    margin: auto;
+    width: 5rem;
+    height: 2rem;
+    font-weight: 600;
+    border: 1px solid #0cb1f3;
+  }
+  .white {
+    color: #0cb1f3;
+    background-color: #fff;
+    margin-right: 10px;
+  }
+  .blue {
+    color: #fff;
+    background-color: #0cb1f3;
   }
 `;
